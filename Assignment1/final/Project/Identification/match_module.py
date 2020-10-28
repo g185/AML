ff87950a281183120863e59a01229a9c9b2dbfff
Gabriele2396 <56448402+Gabriele2396@users.noreply.github.com>
2020-10-28 23:56:30 +0100

    3.b done, show_neighbours

--- Assignment1/final/Project/Identification/match_module.py
+++ Assignment1/final/Project/Identification/match_module.py
@@ -30,12 +30,13 @@ def find_best_match(model_images, query_images, dist_type, hist_type, num_bins):
     
     model_hists = compute_histograms(model_images, hist_type, hist_isgray, num_bins)
     query_hists = compute_histograms(query_images, hist_type, hist_isgray, num_bins)
-    
-    D = np.zeros((len(model_images), len(query_images)))
+
+
+    D = np.zeros((len(query_images), len(model_images)))
         
-    for i in range(len(model_images)):
-        for j in range(len(query_images)):
-            D[i][j] = dist_module.get_dist_by_name( model_hists[i], query_hists[j], dist_type)
+    for i in range(len(query_images)):
+        for j in range(len(model_images)):
+            D[i][j] = dist_module.get_dist_by_name( model_hists[j], query_hists[i], dist_type)
             
     best_match = np.argmin(D, axis=0)
     return best_match, D
@@ -69,10 +70,22 @@ def compute_histograms(image_list, hist_type, hist_isgray, num_bins):
 
 def show_neighbors(model_images, query_images, dist_type, hist_type, num_bins):
     
-    
-    plt.figure()
+    _, D = find_best_match(model_images,query_images, dist_type, hist_type, num_bins)
 
     num_nearest = 5  # show the top-5 neighbors
     
-    #... (your code here)
+    top_k = np.zeros((len(query_images), 5), dtype = int)
+
+    for i in range(len(query_images)):
+        
+        top_k[i] = np.argsort(D[i])[:5]
+        
+        plt.figure()
+        plt.subplot(1,6,1); plt.imshow(np.array(Image.open(query_images[i])), vmin=0, vmax=255); plt.title(query_images[i])
+        plt.subplot(1,6,2); plt.imshow(np.array(Image.open(model_images[top_k[i][0]]))); plt.title(model_images[top_k[i][0]])
+        plt.subplot(1,6,3); plt.imshow(np.array(Image.open(model_images[top_k[i][1]]))); plt.title(model_images[top_k[i][1]])
+        plt.subplot(1,6,4); plt.imshow(np.array(Image.open(model_images[top_k[i][2]]))); plt.title(model_images[top_k[i][2]])
+        plt.subplot(1,6,5); plt.imshow(np.array(Image.open(model_images[top_k[i][3]]))); plt.title(model_images[top_k[i][3]])
+        plt.subplot(1,6,6); plt.imshow(np.array(Image.open(model_images[top_k[i][4]]))); plt.title(model_images[top_k[i][4]])
+        plt.show()
 
