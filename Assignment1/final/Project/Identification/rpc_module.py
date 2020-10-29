6fb7e046adcbb52c1aac494d6166c7a0502aac3d
g185 <giuliano.martinelli97gmail.com>
2020-10-29 15:19:15 +0100

    4.a

--- Assignment1/final/Project/Identification/rpc_module.py
+++ Assignment1/final/Project/Identification/rpc_module.py
@@ -29,27 +29,22 @@ def plot_rpc(D, plot_color):
     sortidx = d.argsort()
     d = d[sortidx]
     l = l[sortidx]
-    
+
+
     tp = 1
     fp = 0
-    fn = 0
+    true = sum(l)
 
-    tresh = np.linspace(0,1,20)
-    
-    for t in tresh:
+    for idt in range(len(d)):
 
-        for idt in range(len(d)):
-            tp += 1 if l[idt] == 1 and d[idt] < t else 0
-            fp += 1 if l[idt] != 1 and d[idt] < t else 0
-            fn += 1 if l[idt] == 1 and d[idt] > t else 0
-            
+        tp += l[idt]
+        fp += 1 - l[idt]    
             
         #Compute precision and recall values and append them to "recall" and "precision" vectors
-        recall.append(tp/(tp+fn))
+        recall.append(tp/true)
         precision.append(tp/(tp+fp))
-        tp, fp, fn = 1,0,0     
 
-    
+
     plt.plot([1-precision[i] for i in range(len(precision))], recall, plot_color+'-')
 
 
@@ -64,7 +59,6 @@ def compare_dist_rpc(model_images, query_images, dist_types, hist_type, num_bins
 
         plot_rpc(D, plot_colors[idx])
     
-    print("as")
     plt.axis([0, 1, 0, 1]);
     plt.xlabel('1 - precision');
     plt.ylabel('recall');
